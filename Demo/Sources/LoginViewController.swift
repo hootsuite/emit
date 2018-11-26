b8b2022a6904c5bf9d5a589df181de97a65374c8
Kerr Marin Miller <kerr.miller@hootsuite.com>
2018-11-26 15:28:01 +0000

    CMOB-1594: Remove char

--- Demo/Sources/LoginViewController.swift
+++ Demo/Sources/LoginViewController.swift
@@ -1,4 +1,4 @@
-d// Copyright © 2018 Hootsuite.
+// Copyright © 2018 Hootsuite.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
